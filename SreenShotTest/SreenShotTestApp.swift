86cf0f11ef71294c0aa67338153ce5202dc6f2ca
Dong487 <Dsheng7777777@gmail.com>
2022-07-28 23:32:56 +0800

    Initial Commit

---
+++ SreenShotTest/SreenShotTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SreenShotTestApp.swift
+//  SreenShotTest
+//
+//  Created by DONG SHENG on 2022/7/28.
+//
+
+import SwiftUI
+
+@main
+struct SreenShotTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
